7dc3907a7d4fb104dfbe917a246ab10239ca11df
Darryl Browne <darryl.browne@i-com.net>
2020-02-05 14:09:00 +0000

    Adds production update script

---
+++ scripts/update-production.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+read -p "Have you run the export-app.sh script first? [Y/n] " -n 1 -r
+echo
+
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    rm -rf production
+    git clone $(git config remote.origin.url) production
+    cd production
+    git checkout production
+
+    git rm -rf .
+    cd ..
+    rsync -av backup/app/ production/
+    cd production
+    rm .gitignore
+    echo "pub/media\nvar" >> .gitignore
+    git add .
+    git commit -m 'Update production'
+    git push --set-upstream origin production
+    cd ..
+    rm -rf production
+fi
