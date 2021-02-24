8ce22e96da67510f7869289ebb1cd14685211256
chachakawooka <chachakawooka@googlemail.com>
2021-02-24 16:58:22 +0000

    updates to 2.4.2

--- docker/stack/start.sh
+++ docker/stack/start.sh
@@ -23,63 +23,47 @@ white="$(tput setaf 7)"
 
 until nc -z -v -w30 ${MAGE_DB_HOST} 3306
 do
-  echo "${yellow}Waiting for database connection...${normal}"
-  # wait for 5 seconds before check again
-  sleep 5
+    echo "${yellow}Waiting for database connection...${normal}"
+    # wait for 5 seconds before check again
+    sleep 5
+done
+
+until nc -z -v -w30 es01 9200
+do
+    echo "${yellow}Waiting for elastic connection...${normal}"
+    # wait for 5 seconds before check again
+    sleep 5
 done
 
 ######################
 # INSTALL MAGENTO
 ######################
-echo "${blue}${bold}php bin/magento setup:config:set \
-    --db-host ${MAGE_DB_HOST} \
-    --db-name ${MAGE_DB_NAME} \
-    --db-user ${MAGE_DB_USER} \
-    --db-password=${MAGE_DB_PASSWORD}${normal}"
+php bin/magento setup:config:set \
+--db-host ${MAGE_DB_HOST} \
+--db-name ${MAGE_DB_NAME} \
+--db-user ${MAGE_DB_USER} \
+--db-password=${MAGE_DB_PASSWORD} \
 
-php bin/magento setup:config:set \
-    --db-host ${MAGE_DB_HOST} \
-    --db-name ${MAGE_DB_NAME} \
-    --db-user ${MAGE_DB_USER} \
-    --db-password=${MAGE_DB_PASSWORD}
-  
-php bin/magento setup:config:set \
-      --cache-backend=redis \
-      --cache-backend-redis-server=redis \
-      --cache-backend-redis-db=0
-
-php bin/magento setup:config:set \
-      --session-save=redis \
-      --session-save-redis-host=redis \
-      --session-save-redis-log-level=3 \
-      --session-save-redis-db=1
 
 MAGENTO_STATUS=$(bin/magento setup:db:status)
 if [[ $MAGENTO_STATUS =~ "Magento application is not installed."$ ]]; then
-echo "${blue}${bold}INSTALLING MAGENTO CORE${normal}"
-
-echo "${blue}${bold}php bin/magento setup:install \
+    
+    php bin/magento setup:install \
     --admin-firstname $MAGENTO_FIRST_NAME \
     --admin-lastname $MAGENTO_LAST_NAME \
     --admin-email $MAGENTO_EMAIL \
     --admin-user $MAGENTO_USER \
     --admin-password $MAGENTO_PASSWORD \
-    --backend-frontname $MAGE_ADMIN_URL${normal}"
-
-php bin/magento setup:install \
-    --admin-firstname $MAGENTO_FIRST_NAME \
-    --admin-lastname $MAGENTO_LAST_NAME \
-    --admin-email $MAGENTO_EMAIL \
-    --admin-user $MAGENTO_USER \
-    --admin-password $MAGENTO_PASSWORD \
-    --backend-frontname $MAGE_ADMIN_URL
-
-
-php bin/magento cache:enable
+    --backend-frontname $MAGE_ADMIN_URL \
+    --elasticsearch-host=es01
+    
+    php bin/magento setup:config:set --page-cache=redis --page-cache-redis-server=redis --page-cache-redis-db=1
+    
+    php bin/magento cache:enable
 else
-php bin/magento setup:db:status
-export ME=$?
-echo "${blue}${bold}DB STATUS: $ME ${normal}"
+    php bin/magento setup:db:status
+    export ME=$?
+    echo "${blue}${bold}DB STATUS: $ME ${normal}"
 fi
 
 
@@ -88,8 +72,8 @@ fi
 ######################
 export IFS=";"
 for repo in ${COMPOSER_REPOS}; do
-  echo "${blue}${bold}ADDING REPO ${cyan}${repo} ${normal}"
-  composer config repositories.$repo composer https://$repo 
+    echo "${blue}${bold}ADDING REPO ${cyan}${repo} ${normal}"
+    composer config repositories.$repo composer https://$repo
 done
 
 ######################
@@ -99,8 +83,8 @@ composer config --global http-basic.repo.magento.com ${MAGENTO_COMPOSER_USER} ${
 
 export IFS=";"
 for module in ${COMPOSER_MODULES}; do
-  echo "${blue}${bold}INSTALLING ${cyan}${module} ${normal}"
-  composer require $module
+    echo "${blue}${bold}INSTALLING ${cyan}${module} ${normal}"
+    composer require $module
 done
 
 
@@ -110,7 +94,7 @@ done
 chmod -R ugoa+rwX var vendor generated pub/static pub/media app/etc
 
 php bin/magento setup:upgrade
-php bin/magento setup:di:compile 
+php bin/magento setup:di:compile
 
 chgrp -R www-data pub
 chmod -R g+rwX pub
--- start.sh
+++ start.sh
@@ -38,42 +38,15 @@ done
 ######################
 # INSTALL MAGENTO
 ######################
-echo "${blue}${bold}php bin/magento setup:config:set \
-    --db-host ${MAGE_DB_HOST} \
-    --db-name ${MAGE_DB_NAME} \
-    --db-user ${MAGE_DB_USER} \
---db-password=${MAGE_DB_PASSWORD} \
---es-hosts=es01:9200 ${normal}"
-
 php bin/magento setup:config:set \
 --db-host ${MAGE_DB_HOST} \
 --db-name ${MAGE_DB_NAME} \
 --db-user ${MAGE_DB_USER} \
---db-password=${MAGE_DB_PASSWORD} \
---es-hosts=es01:9200
-
-php bin/magento setup:config:set \
---cache-backend=redis \
---cache-backend-redis-server=redis \
---cache-backend-redis-db=0
-
-php bin/magento setup:config:set \
---session-save=redis \
---session-save-redis-host=redis \
---session-save-redis-log-level=3 \
---session-save-redis-db=1
+--db-password=${MAGE_DB_PASSWORD}
 
 MAGENTO_STATUS=$(bin/magento setup:db:status)
 if [[ $MAGENTO_STATUS =~ "Magento application is not installed."$ ]]; then
     echo "${blue}${bold}INSTALLING MAGENTO CORE${normal}"
-    
-    echo "${blue}${bold}php bin/magento setup:install \
-    --admin-firstname $MAGENTO_FIRST_NAME \
-    --admin-lastname $MAGENTO_LAST_NAME \
-    --admin-email $MAGENTO_EMAIL \
-    --admin-user $MAGENTO_USER \
-    --admin-password $MAGENTO_PASSWORD \
-    --backend-frontname $MAGE_ADMIN_URL${normal}"
     
     php bin/magento setup:install \
     --admin-firstname $MAGENTO_FIRST_NAME \
@@ -81,7 +54,10 @@ if [[ $MAGENTO_STATUS =~ "Magento application is not installed."$ ]]; then
     --admin-email $MAGENTO_EMAIL \
     --admin-user $MAGENTO_USER \
     --admin-password $MAGENTO_PASSWORD \
-    --backend-frontname $MAGE_ADMIN_URL
+    --backend-frontname $MAGE_ADMIN_URL \
+    --elasticsearch-host=es01
+    
+    php bin/magento setup:config:set --page-cache=redis --page-cache-redis-server=redis --page-cache-redis-db=1
     
     
     php bin/magento cache:enable
@@ -118,8 +94,8 @@ cd /app
 # PERMISSIONS
 ######################
 chmod -R ugoa+rwX var vendor generated pub/static pub/media app/etc
-chgrp -R www-data pub var 
-chmod -R g+rwX pub var 
+chgrp -R www-data pub var
+chmod -R g+rwX pub var
 chmod 777 auth.json
 php bin/magento c:f
 
